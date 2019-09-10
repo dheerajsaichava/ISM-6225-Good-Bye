177ba358727ca124c16a60d9c417c7cd65f2aec9
dheerajsai chava <dheerajsaichava@gmail.com>
2019-09-10 16:24:30 -0400

    Add project files.

---
+++ ISM 6225-Good Bye/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ISM_6225_Good_Bye
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Dheeraj Sai");
+            Console.WriteLine("Good bye world");
+        }
+    }
+}
